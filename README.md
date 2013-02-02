a7305c77877e3ecc9a39b7d01685f61d9ac060c4
Joe Blau <josephblau@gmail.com>
2013-02-01 18:56:22 -0800

    Added short parser and custom code handler

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # README #
 
-coming soon
+### About ###
+
+JJB URL Shortner
+
+http://jjbshort.appspot.com
+
+### How to use ###
+
+* A POST /shorten_url endpoint with json payload {long_url:..., custom_short_code(optional): ...} -> returns {success: true/false, short_code} [Raise an error if the user asks for a custom code but cannot be assigned. If custom is not requested, assign a alphanumeric code of length 6]
+* A /{custom_short_code} endpoint which redirects to a long url
+
+### Tradeoffs ###
+
+
+
+### Testing ###
+```
+$ sudo easy_install gaetestbed
+$ sudo easy_install nose
+$ sudo easy_install nosegae
+$ sudo easy_install webtest
+```
--- main.py
+++ main.py
@@ -15,7 +15,9 @@
 # limitations under the License.
 #
 import webapp2
+import urlparse
 import simplejson as json
+import models
 
 class MainHandler(webapp2.RequestHandler):
   def get(self):
@@ -29,33 +31,63 @@ class ShortenURLHandler(webapp2.RequestHandler):
     data = None
     try: 
       data = json.loads(self.request.body)
+      counterModel = models.Counter.get_or_insert('SHORT')
+      shortCode = None
+      notCustomCode = True
+      originalURL = None
+      
+      # Handle long URL
+      if type(data) is dict and data.has_key('long_url') and len(data['long_url']) > 0:
+        # make the URL short
+        originalURL = str(data['long_url'])
+        shortCode = counterModel.ShortCounter
+      
+      # Handle custom code  
+      if type(data) is dict and data.has_key('custom_short_code') and len(data['custom_short_code']) > 0:
+        shortCode = str(data['custom_short_code'])
+        notCustomCode = False
+      
+      # Parse base URI
+      baseURI = urlparse.urlparse(self.request.url)
+      shortURL = baseURI.scheme + "://" + baseURI.netloc + "/" + str(shortCode)
+      returnData = {
+        'success': True,
+        'short_code': shortURL
+      }
+      # Save URL Model
+      models.ShortLink(
+        OriginalURL=originalURL,
+        ShortURL=shortURL
+      ).put()
+      # Increment Counter
+      if notCustomCode:
+        counterModel.ShortCounter+=1
+        counterModel.put()
+        
+      # Generate Response  {success: true/false, short_code}
+      self.response.write(returnData)
+      return
     except Exception, error:
       self.response.write(error)
-    # Check Data 
+      return
     
-    # Generate Response  {success: true/false, short_code}
     self.response.write(data)
+    return
     
 class CustomShortCodeHandler(webapp2.RequestHandler):
   def get(self):
-    self.response.write('Custom Short Code')
-  def post(self):   
-    self.response.write('Custom Short Code')
-
-class NotFoundPageHandler(webapp2.RequestHandler):
-  def head(self):
-    self.get()
-    self.response.clear()
-  def get(self):
-    self.error(404)
-    self.response.write('404')
-  def post(self):
-    self.error(404)
-    self.response.write('404')    
+    try:
+      shortLink = models.ShortLink.all().filter('ShortURL = ', self.request.uri).get()
+      if shortLink == None:
+        self.response.write('Short URL not found')
+      self.redirect(str(shortLink.OriginalURL))
+      return
+    except Exception, error:
+      self.response.write(error)
+      return
 
 app = webapp2.WSGIApplication([
   ('/', MainHandler),
   ('/shorten_url', ShortenURLHandler),
-  ('/*', CustomShortCodeHandler),
-  ('/*', NotFoundPageHandler)
+  ('/.*', CustomShortCodeHandler)
 ], debug=True)
